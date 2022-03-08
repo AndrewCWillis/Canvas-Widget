d7b7ce6c732b319a59aed6fa709ce9063720ed01
Andrew C. Willis <acwi257@g.uky.edu>
2022-03-07 19:34:28 -0500

    some more change on graphics, made modular

--- canvasPractice.py
+++ canvasPractice.py
@@ -1,42 +1,77 @@
+'''
+CS498 Group Project 
+Gathering relevant information from the Canvas API with Token
+
+
+'''
 from canvasapi import Canvas
+import os
 import re
+import cryptography 
 
 TOKEN = '1139~bfXGBZAZHKoh7ZqH4GrUtoATy9M37hrTxhQNZ5JR4WR8Ycr6Zjm3vhU1VO7b5RT8'
 
-URL = 'https://uk.instructure.com'
 
-canvas_api = Canvas(URL, TOKEN)
+class userCanvas:
+    def __init__(self):
+        URL = 'https://uk.instructure.com'
+        if os.environ.get('Canvas_Key') is None:
+            print('no key stored')
+            #generate a key?
+        if os.environ.get('Canvas_Token') is None:
+            print('encrypted token not stored')
+            #store encrypted token?
+        self.canvas_api = Canvas(URL, TOKEN)
+        self.user = self.canvas_api.get_current_user()
+        self.courses = self.user.get_favorite_courses(enrollment_state = 'active')
+        self.courseIds = []
+        self.announcements = self.canvas_api.get_announcements(context_codes = [course for course in self.courses])
+        self.todo = self.canvas_api.get_todo_items()
+        for course in self.courses:
+            print(course)
+            self.courseIds.append(str(course.id))
+            print('Color in Canvas: '+str(course.course_color))
+            self.courseIds.append(course.id)
+            calendar = course.calendar
+            print(calendar)
+            self.assignments = course.get_assignments()
+            for assignment in self.assignments:
+                print('\t'+str(assignment)+' Due At: '+(str(assignment.due_at_date) if hasattr(assignment, 'due_at_date') else ''))
+        print('-------------------------------')
 
-user = canvas_api.get_current_user()
+        print('Notifications: ')
 
-courses = user.get_favorite_courses(enrollment_state = 'active')
-courseIds = []
-for course in courses:
-    print(course)
-    courseIds.append(course.id)
-    calendar = course.calendar
-    print(calendar)
-    assignments = course.get_assignments()
-    for assignment in assignments:
-        print('\t'+str(assignment)+' Due At: '+(str(assignment.due_at_date) if hasattr(assignment, 'due_at_date') else ''))
-    print('-------------------------------')
+    def getAnnouncements(self):
+        output = ""
+        for announcement in self.announcements:
+            output += str(announcement)+'\n'
+            print('\t'+str(announcement))
+        return output
+    def getToDo(self):
+        output = ""
+        print('\nToDo: ')  
+        for task in self.todo:
+            output += str(task.assignment['name']) + '\n\t' +str(task.assignment['due_at'])
+            print(task.assignment['name'])
+            print('\t'+str(task.assignment['due_at']))
+        return output
+    def getGrades(self):
+        
+        print('Grade:')
+        enrollments = self.user.get_enrollments()
+        output = ""
+        for enrollment in enrollments:
+            if enrollment.course_id in self.courseIds:
+                output += str(enrollment.course_id)+": "+str(enrollment.grades['current_grade']) if hasattr(enrollment, 'grades') and 'current_grade' in enrollment.grades else ''
+                output += '\n'
+                print(str(enrollment.course_id)+": "+str(enrollment.grades['current_grade']) if hasattr(enrollment, 'grades') and 'current_grade' in enrollment.grades else '')
+        return output
+    def getColors(self):
+        colors = self.user.get_colors()
+        Colors = {courseID[7:]:color for (courseID, color) in colors['custom_colors'].items() if courseID[7:] in self.courseIds }
+        print(Colors)
 
-print('Notifications: ')
+'''
+Playing with encryption of Token
+'''
 
-for event in calendar:
-    print(type(event))
-    
-announcements = canvas_api.get_announcements(context_codes = [course for course in courses])
-
-for announcement in announcements:
-    print('\t'+str(announcement))
-print('\nToDo: ')  
-todo = canvas_api.get_todo_items()
-for task in todo:
-    print(task.assignment['name'])
-    print('\t'+str(task.assignment['due_at']))
-print('Grade:')
-enrollments = user.get_enrollments()
-for enrollment in enrollments:
-    if enrollment.course_id in courseIds:
-        print(str(enrollment.course_id)+": "+str(enrollment.grades['current_grade']) if hasattr(enrollment, 'grades') and 'current_grade' in enrollment.grades else '')
--- graphicsBasic.py
+++ graphicsBasic.py
@@ -8,6 +8,7 @@ import canvasPractice
 import tkinter as tk
 from tkinter import ttk
 
+stuCanvas = canvasPractice.userCanvas()
 root = tk.Tk()
 root.tk.call('lappend', 'auto_path', 'awthemes-10.4.0')
 root.tk.call('package', 'require', 'awdark')
@@ -20,18 +21,33 @@ tabControl = ttk.Notebook(root)
   
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
-  
+tab3 = ttk.Frame(tabControl)
+tab4 = ttk.Frame(tabControl)
+
 tabControl.add(tab1, text ='Grades')
-tabControl.add(tab2, text ='Tab 2')
+tabControl.add(tab2, text ='Announcements')
+tabControl.add(tab3, text ='Assignments')
+tabControl.add(tab4, text ='To Do')
+
 tabControl.pack(expand = 1, fill ="both")
   
 ttk.Label(tab1, 
-          text ="Welcome to Canvas").grid(column = 0, 
+          text = stuCanvas.getGrades()).grid(column = 0, 
                                row = 0,
                                padx = 30,
                                pady = 30)  
 ttk.Label(tab2,
-          text ="Blah").grid(column = 0,
+          text = stuCanvas.getAnnouncements()).grid(column = 0,
+                                    row = 0, 
+                                    padx = 30,
+                                    pady = 30)
+ttk.Label(tab3,
+          text = stuCanvas.getAnnouncements()).grid(column = 0,
+                                    row = 0, 
+                                    padx = 30,
+                                    pady = 30)
+ttk.Label(tab4,
+          text = stuCanvas.getToDo()).grid(column = 0,
                                     row = 0, 
                                     padx = 30,
                                     pady = 30)
